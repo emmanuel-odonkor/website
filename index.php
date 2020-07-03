20892af78cb988f062ce03926332883f96c6c2cb
emmanuel-odonkor <46679289+emmanuel-odonkor@users.noreply.github.com>
2020-07-03 08:55:27 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>BOOK REVIEW--HOME</title>
+</head>
+<body>
+	<div class="head" style="background-image: url(images/mac.png);">
+		<center><h1>WELCOME TO THE BOOKS REVIEW WEBSITE</h1>
+		<a href="index.php" style="color:blue;"><b>HOME</b> || </a>
+		<a href="#about-us" style="color: blue;"><b>ABOUT SITE</b></a></center>
+	
+    <marquee direction="left"><h2>This site allows you to review some textbooks</h2></marquee>
+
+<!-- SECTION FOR THE TABLE -->
+<section class="sect">
+<center><table border="8px" style="margin-top: 90px;" cellspacing="10px" cellpadding="10px">
+	<tr>
+		<th>BOOK TITLE</th>
+		<th>BOOK DETAILS</th>
+	</tr>
+	<tr>
+		<td>DATA STRUCTURES AND ALGORITHMS</td>
+		<td><a href="#data_structures">DETAILS</a></td>
+	</tr>
+	<tr>
+		<td>PYTHON PROGRAMMING</td>
+		<td><a href="#python">DETAILS</a></td>
+	</tr>
+</table></center>
+</section>
+	<br />
+	<br />
+   </div>
+
+  <!-- ABOUT US SECTION -->
+<section id="about-us">
+	<center><h1>ABOUT SITE</h1>
+	<h3 style="color:blue;">GET TO KNOW MORE ABOUT THIS WEBSITE</h3></center>
+	<br />
+	<center><h3><b>This site gives you the opportunity to view,analyze<br />
+  	course materials. These core materials are lie within<br />
+  	the branch of Computer Science.</b></h3></center>
+
+	<center><a href="#data_structures"><button>VIEW DETAILS FOR DATA STRUCTURES</button></a></center>
+  <br />
+  <br />
+  <center><a href="#python" ><button>VIEW DETAILS FOR PYTHON PROGRAMMING</button></a></center><br />
+</section>
+
+<!-- DATA STRUCTURES BOOK REVIEW SECTION -->
+<section style="background-image: url(images/emma2.jpg); background-repeat: no-repeat;" id="data_structures">
+	<center><h1>DATA STRUCTURES AND ALGORITHMS</h1>
+	<br />
+	<hr />
+	<h3>DATA STRUCTURES AND ALGORITHMS (THIRD EDITION)</h3>                     
+	<a href="#"><img src="images/dstructures.jpeg" width="300px" height="400px" /></a>
+	<h3>AUTHORS:</h3>
+	<ul>
+	<li>Michael T. Goodrich</li>
+	<li>Roberto Tamassia</li>
+	</ul>
+	<br />
+</section>
+
+<!--PYTHON PROGRAMMING FOR CS REVIEW SECTION -->
+<section style="background-image: url(images/emma2.jpg); background-repeat: no-repeat;" id="python">
+	<center><h1>PYTHON PROGRAMING FOR CS</h1>
+	<br />
+	<h3>PYTHON PROGRAMMING (THIRD EDITION)</h3>                     
+	<a href="#"><img src="images/python.jpg" width="300px" height="400px"/></a>
+	<h3>AUTHOR:</h3>
+	<ul>
+	<li>John M. Zelle</li>
+	</ul>
+	<br />
+</section>
+
+<!-- FOOTER SECTION -->
+<footer style="background-color: grey;">
+	<br />
+	<b><p><center>POSTED BY: ODONKOR EMMANUEL TEYE-KOFI</center></p>
+	<p><center>SOFTWARE DEVELOPMENT </center></p></b>
+</footer>
+
+</body>
+</html>
